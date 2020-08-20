3d5a7865542e597d9795ddfc1eeb246cc369c815
Benjamin Aragon <baragon23@gmail.com>
2020-08-19 19:47:43 -0700

    'under construction' added to Browse page

--- src/components/Pages/Browse.js
+++ src/components/Pages/Browse.js
@@ -3,11 +3,7 @@ import React from 'react';
 const Browse = () => {
 	return (
 		<div>
-			<h1>Browse By System</h1>
-			<div>PS4</div>
-			<div>PS3</div>
-			<div>Switch</div>
-			<div>Xbox One</div>
+			<h1>Under Construction</h1>
 		</div>
 	);
 };
