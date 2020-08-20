665297accd9d8d9fbec3842dc45cf8434dd9bee5
Benjamin Aragon <baragon23@gmail.com>
2020-08-19 19:59:27 -0700

    home page title drop shadow added

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -18,10 +18,13 @@ const useStyles = makeStyles({
 		alignItems: 'center',
 		'& svg': {
 			height: '3em',
+			marginRight: '1em',
+			filter: 'drop-shadow(5px 5px 0px #00695c)',
 			width: '3em',
 		},
 		'& h3': {
 			margin: '0',
+			textShadow: '4px 3px 0 #00695c, 7px 5px 0 rgba(0, 0, 0, 0.15)',
 		},
 	},
 	subTitle: {
