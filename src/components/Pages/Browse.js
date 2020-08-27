8a1e393d4b9fe2eebb8cc83e3d529a80df091ff3
Benjamin Aragon <baragon23@gmail.com>
2020-08-26 18:37:14 -0700

    removed warnings to fix compile error

--- src/components/Pages/Browse.js
+++ src/components/Pages/Browse.js
@@ -1,20 +1,9 @@
 import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 
-import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles((theme) => ({
-	intro: {
-		margin: '2em 0',
-		width: '100%',
-		height: '100%',
-		padding: '2em',
-	},
-}));
-
-const systems = [
+/*const systems = [
 	'3DO',
 	'Atari Jaguar',
 	'Dreamcast',
@@ -47,11 +36,9 @@ const systems = [
 	'Xbox',
 	'Xbox 360',
 	'Xbox One',
-];
+];*/
 
 const Browse = () => {
-	const classes = useStyles();
-
 	return (
 		<Fragment>
 			<Typography variant="h5">Browse By Game System</Typography>
